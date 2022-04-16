0c3a41a619929ee7b72e9ad7ec289f84ab592af8
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-16 15:49:52 +0900

    完了

--- data10.py
+++ data10.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from turtle import color
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,14 @@ from mlxtend.plotting import heatmap
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import RANSACRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
                  'python-machine-learning-book-3rd-edition'
@@ -160,6 +169,180 @@ plt.xlabel('Predicted values')
 plt.ylabel('Residuals')
 plt.legend(loc='upper left')
 plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+plt.xlim([-10, 50])
+plt.tight_layout()
+plt.show()
+
+# 平均二乗誤差を出力
+print('MSE train: %.3f,test: %.3f' % (mean_squared_error(y_train, y_train_pred),
+                                      mean_squared_error(y_test, y_test_pred)))
+
+# R^2(決定係数)のスコアを出力
+print('R^2 train: %.3f, test: %.3f' % (r2_score(y_train, y_train_pred),
+                                       r2_score(y_test, y_test_pred)))
+
+# L2ペナルティ項の影響度合いを表す値を引数に指定
+ridge = Ridge(alpha=1.0)
+lasso = Lasso(alpha=1.0)
+elanet = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+X = np.array([258.0, 270.0, 294.0, 320.0, 342.0, 368.0, 396.0, 446.0, 480.0,
+             586.0])[:, np.newaxis]
+
+y = np.array([236.4, 234.4, 252.8, 298.6, 314.2,
+             342.2, 360.8, 368.0, 391.2, 390.8])
+# 線形回帰（最小二乗法）モデルのクラスをインスタンス化
+lr = LinearRegression()
+pr = LinearRegression()
+# 2次の多項式特徴量のクラスをインスタンス化
+quadratic = PolynomialFeatures(degree=2)
+X_quad = quadratic.fit_transform(X)
+lr.fit(X, y)
+# np.newaxisで列ベクトルにする
+X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+# 予測値を計算
+y_lin_fit = lr.predict(X_fit)
+pr.fit(X_quad, y)
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+
+# 散布図、線形回帰モデル、多項式回帰モデルの結果をプロット
+plt.scatter(X, y, label='Training points')
+plt.plot(X_fit, y_lin_fit, label='Linner fit', linestyle='--')
+plt.plot(X_fit, y_quad_fit, label='Quadratic fit')
+plt.xlabel('Explanatory variable')
+plt.ylabel('Predited or known target values')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+y_lin_pred = lr.predict(X)
+y_quad_pred = pr.predict(X_quad)
+print('Training MSE linear: %.3f,quadratic: %.3f' %
+      (mean_squared_error(y, y_lin_pred), mean_squared_error(y, y_quad_pred)))
+
+print('Training R^2 linner: %.3f,quadratic: %.3f' %
+      (r2_score(y, y_lin_pred), r2_score(y, y_quad_pred)))
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+regr = LinearRegression()
+
+# 2次と3次の特徴量を作成
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
+
+# 特徴量の学習、予測、決定係数の計算
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
+
+# 2次の特徴量の学習、予測、決定係数の計算
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+# 3次の特徴量の学習、予測、決定係数の計算
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+# 各モデルの結果をプロット
+plt.scatter(X, y, label='Training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='Linear (d=1), $R ^ 2= % .2f$' %
+         linear_r2, color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_quad_fit, label='Quadratic (d=2), $R^2=%.2f$' %
+         quadratic_r2, color='red', lw=2, linestyle='--')
+plt.plot(X_fit, y_cubic_fit, label='Cubic (d=3), $R^2=%.2f$' %
+         cubic_r2, color='green', lw=2, linestyle='--')
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper right')
+plt.show()
+
+# 特徴量を変換
+X_log = np.log(X)
+y_sqrt = np.log(y)
+
+# 特徴量の学習、予測、決定係数の計算
+X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+regr = regr.fit(X_log, y_sqrt)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+# 射影したデータを使った学習結果をプロット
+plt.scatter(X_log, y_sqrt, label='Training points', color='lightgray')
+plt.plot(X_fit, y_lin_fit, label='Linner (d=1), $R^2=%.2f$' %
+         linear_r2, color='blue', lw=2)
+plt.xlabel('log(% lower status of the population [LSTAT]')
+plt.ylabel('$\sqrt{Price \; in \; \$1000s [MEDV]}$')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+# 決定木回帰モデルのクラスをインスタンス化：max_depthで決定木の深さを指定
+tree = DecisionTreeRegressor(max_depth=3)
+tree.fit(X, y)
+# argsortはソート後のインデックスを返し、flattenは1次元の配列を返す
+sort_idx = X.flatten().argsort()
+# lin_replot関数により、散布図と回帰直線を作成
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.show()
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.4, random_state=1)
+
+# ランダムフォレスト回帰のクラスをインスタンス化
+forest = RandomForestRegressor(n_estimators=1000,
+                               criterion='mse',
+                               random_state=1,
+                               n_jobs=-1)
+
+forest.fit(X_train, y_train)
+y_train_pred = forest.predict(X_train)
+y_test_pred = forest.predict(X_test)
+# MSE（平均二乗誤差）を出力
+print('MSE train: %.3f,test:%.3f' % (
+    mean_squared_error(y_train, y_train_pred),
+    mean_squared_error(y_test, y_test_pred)
+))
+
+# R^2（決定係数）を出力
+print('R^2 train: %.3f,test: %.3f' % (r2_score(y_train, y_train_pred),
+                                      r2_score(y_test, y_test_pred)))
+
+
+# 予測値と残差をプロット
+plt.scatter(y_train_pred,
+            y_train_pred-y_train,
+            c='steelblue',
+            edgecolors='white',
+            marker='o',
+            s=35,
+            alpha=0.9,
+            label='Training data')
+
+plt.scatter(y_test_pred,
+            y_test_pred-y_test,
+            c='limegreen',
+            edgecolors='white',
+            marker='s',
+            s=35,
+            alpha=0.9,
+            label='Test data')
+
+plt.xlabel('Predicted values')
+plt.ylabel('Ressiduals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='black')
 plt.xlim([-10, 50])
 plt.tight_layout()
 plt.show()
